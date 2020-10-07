cfd810f7922243ee017830491b85cfe5efc20453
Hamade <mmdarrah@hotmail.com>
2020-10-07 11:37:23 +0200

    Added read from text and check the resutls

--- Guldkortet/Form1.Designer.cs
+++ Guldkortet/Form1.Designer.cs
@@ -30,13 +30,21 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 342);
+            this.button1.Location = new System.Drawing.Point(66, 193);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 59);
+            this.button1.Size = new System.Drawing.Size(79, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "Connect to the server";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,20 +52,79 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 179);
+            this.textBox1.Location = new System.Drawing.Point(66, 158);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(331, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Check your luck";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 59);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(315, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(136, 135);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(184, 47);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(107, 68);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 450);
+            this.ClientSize = new System.Drawing.Size(487, 274);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +134,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Guldkortet/Form1.cs
+++ Guldkortet/Form1.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,52 +15,26 @@ namespace Guldkortet
 
         //the port number
         private int port = 12345;
-        //the clinic
+        //the client
         private TcpClient client;
         //connect the client
         private TcpListener listener;
 
-        
+        //list of string from the class list user
+        Userlist<string> userlist = new Userlist<string>("", "", "");
+        List<Userlist<string>> userInfo = new List<Userlist<string>>();
+        //list of string from the class list user
+        List<Cardlist<string>> cardInfo = new List<Cardlist<string>>();
 
         public Form1()
         {
             InitializeComponent();
             //This metod will run when the program run
             ReadFromTextFile();
+            // The main picture
+            pictureBox3.Load(Application.StartupPath + "\\Grafik\\nos.png");
         }
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            try
-            {
-                //The IP address that the client should connect
-                IPAddress address = IPAddress.Parse("127.0.0.1");
-                //TCPListener object
-                listener = new TcpListener(address, port);
-                //start the listener
-                listener.Start();
-                //Give the button a color to know that there is a connection
-                button1.BackColor = Color.LightGreen;
-                button1.Enabled = false;
-                StartReception();
-                //textBox1.Text = "Test connection";
-                button1.Text = "Connected";
-            }
-            catch (Exception error)
-            {
-                //in case of error
-                MessageBox.Show(error.Message, this.Text);
-                button1.BackColor = Color.Red;
-                button1.Text = "Not connected";
-                return;
-            }
-            
-
-
-        }
-
+        // a privet async function to handel the connection
         private async void StartReception()
         {
             try
@@ -80,6 +51,7 @@ namespace Guldkortet
             StartReading(client);
         }
 
+       
         private async void StartReading(TcpClient k)
         {
             try
@@ -107,10 +79,341 @@ namespace Guldkortet
             //If the customer list text file exists
             if (File.Exists("kundlista.txt"))
             {
-                
+                // Creating a list of strings
+                List<string> items = new List<string>();
+                // StreamReader to load a text, line by line
+                StreamReader reader = new StreamReader("kundlista.txt", Encoding.Default, false);
+                string item = "";
+                // Continue to read until you reach end of file
+                while ((item = reader.ReadLine()) != null)
+                {
+                    items.Add(item);
+                }
+                //Lopping 
+                foreach (string a in items)
+                {
+                    //Put each part in the correct variable
+                    string[] vektor = a.Split(new string[] { "###" }, StringSplitOptions.None);
+                    string SerialNumber = vektor[0];
+                    string Name = vektor[1];
+                    string Location = vektor[2];
+                    //Put all variables in a list
+                    userlist = new Userlist<string>(SerialNumber, Name, Location);
+                    userInfo.Add(userlist);
+
+                }
             }
+
+            else
+            {
+                //in case of error
+                MessageBox.Show("Textfilen hittades inte");
+
+            }
+
+            //If the cards list text file exists
+            if (File.Exists("kortlista.txt"))
+            {
+                // Creating a list of strings
+                List<string> items = new List<string>();
+                // StreamReader to load a text, line by line
+                StreamReader reader = new StreamReader("kortlista.txt", Encoding.Default, false);
+
+                string item = "";
+                // Continue to read until you reach end of file
+                while ((item = reader.ReadLine()) != null)
+                {
+                    items.Add(item);
+                }
+                //Lopping 
+                foreach (string a in items)
+                {
+                    //Put each part in the correct variable
+                    string[] vektor = a.Split(new string[] { "###" }, StringSplitOptions.None);
+                    string CardNumber = vektor[0];
+                    string Reward = vektor[1];
+                    //According to the reward the case will fire
+                    switch (Reward)
+                    {
+                        case "Dunderkatt":
+                            Dunderkatt<string> Dund = new Dunderkatt<string>(CardNumber);
+                            cardInfo.Add(Dund);
+                            break;
+                        case "Kristallhäst":
+                            Kristallhäst<string> Krist = new Kristallhäst<string>(CardNumber);
+                            cardInfo.Add(Krist);
+                            break;
+                        case "Överpanda":
+                            Överpanda<string> över = new Överpanda<string>(CardNumber);
+                            cardInfo.Add(över);
+                            break;
+                        case "Eldtomat":
+                            Eldtomat<string> Eldto = new Eldtomat<string>(CardNumber);
+                            cardInfo.Add(Eldto);
+                            break;
+
+                    }
+
+                }
+
+            }
+            else
+            {
+                //in case of error
+                MessageBox.Show("Textfilen hittades inte");
+
+            }
+        }
+
+        // This button is responsible for connecting to the server
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //The IP address that the client should connect
+                IPAddress address = IPAddress.Parse("127.0.0.1");
+                //TCPListener object
+                listener = new TcpListener(address, port);
+                //start the listener
+                listener.Start();
+                //Give the button a color to know that there is a connection
+                button1.BackColor = Color.LightGreen;
+                button1.Enabled = false;
+                StartReception();
+                //textBox1.Text = "Test connection";
+                button1.Text = "Ansluten";
+            }
+            catch (Exception error)
+            {
+                //in case of error
+                MessageBox.Show(error.Message, this.Text);
+                button1.BackColor = Color.Red;
+                button1.Text = "Inte ansluten";
+                return;
+            }
+
+
+
+        }
+
+        // This button is responsible for check the prize
+        private async void button2_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+
+                //Check if the client is connected so we can send the message
+                if (client.Connected)
+                {
+                    client.NoDelay = true;
+                    byte[] buffer = new byte[2000 * 2000];
+                    string[] vektore = textBox1.Text.Split(new string[] { "-" }, StringSplitOptions.None);
+                    bool user = false;// if the user is correct we will change it to true
+                    bool card = false;// if the card is correct we will change it to true
+
+
+                    //To compaere the data from the card list and the user list we need two loops
+                    for (int c = 0; c < cardInfo.Count; c++)
+                    {
+                        for (int u = 0; u < userInfo.Count; u++)
+                        {
+                            if (userInfo[u].serialNumber == vektore[0] && cardInfo[c].cardNumber == vektore[1])
+                            {
+                                //ToString method will be send to the client
+                                buffer = Encoding.Unicode.GetBytes(userInfo[u].ToString() + cardInfo[c].ToString());
+                                await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+                                //We change the variables to true
+                                user = true;
+                                card = true;
+                                //Two pictures will pop up when the user wins a prize the first is the same for all prizes, the second is unique according to the prize
+                                pictureBox2.Load(Application.StartupPath + "\\Grafik\\grattis.png");
+                                pictureBox1.Load(Application.StartupPath + "\\Grafik\\" + cardInfo[c].reward + ".png");
+
+                            }
+                        }
+                    }
+                    //A loop to find when the user is correct but the card is not
+                    for (int i = 0; i < userInfo.Count; i++)
+
+                    {
+                        if (userInfo[i].serialNumber == vektore[0] && card == false)
+                        {
+                            buffer = Encoding.Unicode.GetBytes(userInfo[i].ToString() + " ditt kodkort är inte rätt försök igen!");
+                            await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+                            user = true;
+                            card = false;
+                        }
+                    }
+
+
+                    //A loop to find when the user is false but the card is true
+                    for (int i = 0; i < cardInfo.Count; i++)
+                    {
+                        if (user == false && cardInfo[i].cardNumber == vektore[1])
+                        {
+                            buffer = Encoding.Unicode.GetBytes(cardInfo[i].ToString() + " men du har inget konto hos oss!");
+                            await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+                            user = false;
+                            card = true;
+                        }
+                    }
+
+
+
+                    //Both user and card is not exist
+                    if (user == false && card == false)
+                    {
+                        buffer = Encoding.Unicode.GetBytes("Kunden och kortet finns inte. Var god försök igen!");
+                        await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+                    }
+
+
+
+                }
+            }
+            catch
+            {
+                //fel meddelande ska visas om någon gick fel
+                MessageBox.Show("Klienten ansluter inte till servern!");
+                return;
+            }
+        }
+
+        // This button is responsible for delet the data inside the textBox
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            //Delete text inside testBox1
+            textBox1.Text = "";
+            //Delete the prize images
+            pictureBox2.Image = null;
+            pictureBox1.Image = null;
+            //Add color when click for half second
+            button3.BackColor = Color.LightSalmon;
+            await Task.Delay(500);
+            button3.BackColor = SystemColors.Control;
+        }
+
+        //The field where the data from server appear
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //A unique picture according to the prize
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //A congratulation picture appear every time the user win a prize
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            //The main picture
         }
     }
 }
 
 
+
+
+
+//cards class list
+class Cardlist<T>
+{
+    // Class properties all are public strings
+    public string cardNumber;
+    public string reward;
+    public Cardlist(string CardNumber)
+    {
+        cardNumber = CardNumber;
+    }
+}
+
+
+
+// Subclass with one more propertie "reward"
+class Dunderkatt<T> : Cardlist<T>
+{
+    public Dunderkatt(string CardNumber) : base(CardNumber)
+    {
+        reward = "Dunderkatt";
+    }
+    //ToString method
+    public override string ToString()
+    {
+        return "Grattis, du har vunnit " + reward + " guldkort.";
+    }
+}
+
+
+
+// Subclass with one more propertie "reward"
+class Kristallhäst<T> : Cardlist<T>
+{
+    public Kristallhäst(string CardNumber) : base(CardNumber)
+    {
+        reward = "Kristallhäst";
+    }
+    //ToString method
+    public override string ToString()
+    {
+        return "Grattis, du har vunnit " + reward + " guldkort.";
+    }
+}
+
+
+// Subclass with one more propertie "reward"
+class Överpanda<T> : Cardlist<T>
+{
+    public Överpanda(string CardNumber) : base(CardNumber)
+    {
+        reward = "Överpanda";
+    }
+    //ToString method
+    public override string ToString()
+    {
+        return "Grattis, du har vunnit " + reward + " guldkort.";
+    }
+}
+
+
+// Subclass with one more propertie "reward"
+class Eldtomat<T> : Cardlist<T>
+{
+    public Eldtomat(string CardNumber) : base(CardNumber)
+    {
+        reward = "Eldtomat";
+    }
+    //ToString method
+    public override string ToString()
+    {
+        return "Grattis, du har vunnit " + reward + " guldkort.";
+    }
+}
+
+
+//cards class list
+class Userlist<T>
+{
+    // Class properties all are public strings
+    public string serialNumber;
+    public string name;
+    public string location;
+    public Userlist(string SerialNumber, string Name, string Location)
+    {
+        serialNumber = SerialNumber;
+        name = Name;
+        location = Location;
+    }
+    //ToString method
+    public override string ToString()
+    {
+        return "Hej " + name + "! ";
+    }
+}
